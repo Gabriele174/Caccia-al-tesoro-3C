393bc0ed69059846cc34f3a86244cd1a648c212d
DePaolaAndrea <162124887+DePaolaAndrea@users.noreply.github.com>
2024-04-03 09:59:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 function doLogin () {
-  var username = document.getElementById("Username").value;
+  var username = document.getElementById("username").value;
   var psw = document.getElementById("password").value;
   if(username == || psw == ""){
       alert("username e password obbligatori");
