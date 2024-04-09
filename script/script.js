a66b476a50da9898afd92d9de010bb57b2808ff9
DePaolaAndrea <162124887+DePaolaAndrea@users.noreply.github.com>
2024-04-09 11:55:05 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,4 +4,22 @@ function doLogin (d) {
   if(username == "" || psw == "")   {
       alert("username e password obbligatori");
   }
+  else {
+          fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  "exampleInputUsername": username,
+                  "exampleInputPassword": password
+              })
+          })
+          .then(response => responsive.json())
+          .then(data => console.log(data))
+          .catch((error) =>{
+              console.error('Error:', error);
+          });
+
+      }
 }
