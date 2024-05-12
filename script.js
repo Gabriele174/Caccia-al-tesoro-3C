1e6288cb9e5adff9e0040f80f7aca022c474af29
Gabriele174 <162124980+Gabriele174@users.noreply.github.com>
2024-05-12 19:13:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ function doLogin () {
       alert("username e password obbligatori");
   }
 }
+function doLogin (event) {
+
+var username = document.getElementById("username").value; var psw = document.getElementById("password").value;
+
+if (username == "" || psw == "") {
+
+alert("Username and password are mandatory.");
+
+} else {
+
+fetch("https://www.cacciaapi.altervista.org/login.php/loginIpad", {
+
+method: "POST"
+
+headers: {
+
+"Content-Type": "application/json",
+
+},
+
+body: JSON.stringify({
+
+"username": username,
+
+"password": psw
+
+})
