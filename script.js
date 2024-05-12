ca0910296c7f5924d13b007fd223fce2531feca3
Gabriele174 <162124980+Gabriele174@users.noreply.github.com>
2024-05-12 19:40:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,32 +5,28 @@ function doLogin () {
   var psw = document.getElementById("password").value;
   if(username == || psw == ""){
       alert("username e password obbligatori");
+  } else {
+    fecth (" ", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringfy({
+        "username": username,
+        "password": psw
+      })
+    })
+     then (response => response.json())
+    .then(data => {
+      console.log(data);
+      window.location.href = "game.html"; // Redirectto game.html
+    })
+
+    .catch((error) => {
+      console.error("Error:". error);
+     });
   }
 }
-function doLogin (event) {
 
-var username = document.getElementById("username").value; var psw = document.getElementById("password").value;
+  
 
-if (username == "" || psw == "") {
-
-alert("Username and password are mandatory.");
-
-} else {
-
-fetch("https://www.cacciaapi.altervista.org/login.php/loginIpad", {
-
-method: "POST"
-
-headers: {
-
-"Content-Type": "application/json",
-
-},
-
-body: JSON.stringify({
-
-"username": username,
-
-"password": psw
-
-})
